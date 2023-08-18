611019502f8c0347c770fef5120d1bbc6a806fb3
Lucane <lucassanntos01@gmail.com>
2023-08-18 02:29:45 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from 'vue'
+  const counter = ref(0)
+</script>
+
+<template>
+  <div id="app">
+    {{ counter }}
+    <button @click="counter += 1">+</button>
+    <button @click="counter -= 1">-</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
